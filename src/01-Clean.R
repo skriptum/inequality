3e8196085bfca76507f29c6bb9847a13262723a5
skriptum <77919093+skriptum@users.noreply.github.com>
2025-06-11 17:12:02 +0200

    gini indices

--- src/01-Clean.R
+++ src/01-Clean.R
@@ -25,53 +25,7 @@ df <- df %>%
 ## Explain 
 
 sort(unique(df$INSTR_ASSET))
-
-# ADA 	Adjusted debt to asset ratio
-# F_NNA Adjusted total assets/liabilities (financial and net non-financial)
-# 
-# BW 	  Business wealth (NUB + F51M)
-# F51M 	Unlisted shares and other equity
-# NUB 	Non-financial business wealth
-# 
-# F4B 	Loans for house purchasing
-# F4X 	Loans other than for house purchasing
-# 
-# F2M 	Deposits
-# F3 	  Debt securities
-# F511 	Listed shares
-# F52 	Investment fund shares/units
-# F62 	Life insurance and annuity entitlements
-# NUN 	Housing wealth (net)
-# 
-# NWA 	Adjusted wealth (net)
-
 sort(unique(df$DWA_GRP))
-
-# _Z  not applicable
-# 
-# B50 Bottom 50% based on net wealth concept
-# D1 	Decile 1 based on net wealth concept
-# D10 Decile 10 based on net wealth concept
-# D2 	Decile 2 based on net wealth concept
-# D3 	Decile 3 based on net wealth concept
-# D4 	Decile 4 based on net wealth concept
-# D5 	Decile 5 based on net wealth concept
-# D6 	Decile 6 based on net wealth concept
-# D7 	Decile 7 based on net wealth concept
-# D8 	Decile 8 based on net wealth concept
-# D9 	Decile 9 based on net wealth concept
-# 
-# HSO Housing status - Owner/partial owner
-# HST Housing status - Tenant/Free use
-# 
-# T10 Top 10% based on net wealth concept
-# T5 	Top 5% based on net wealth concept
-# 
-# WSE Working status - Employee
-# WSR Working status - Retired
-# WSS Working status - Self-employed
-# WSU Working status - Unemployed
-# WSX Working status-Undefined and other
 
 ## Further Cleaning
 
@@ -98,20 +52,23 @@ df$TIME_PERIOD <- as.Date(as.yearqtr(df$TIME_PERIOD, format = "%Y-Q%q"), frac = 
 
 write_csv(df, "../data/DWA_ECB_clean.csv")
 
-## Generate a clean german data
+## Generate a df with simplified variables
 
-df_germany <- df %>%
+df_filter <- df %>%
   filter(
-    REF_AREA == "DE",
     INSTR_ASSET %in% c("F51M", "NUB", "F62", "F52", "F511", "F3", "NUN", "F2M", "NWA", "F4X", "F4B"),
     DWA_GRP %in% c("B50", "D06", "D07", "D08", "D09", "D10")
-    ) %>%
-  select(-c(REF_AREA,KEY,TITLE, COMMENT_TS)) %>%
+  ) %>%
+  select(-c(KEY, TITLE, COMMENT_TS)) %>%
   pivot_wider(
     names_from = INSTR_ASSET,
     values_from = OBS_VALUE
   ) %>%
-  mutate(
+  reframe(
+    REF_AREA = REF_AREA,
+    TIME_PERIOD = TIME_PERIOD,
+    DWA_GRP = DWA_GRP,
+    UNIT_MEASURE = UNIT_MEASURE,
     BW = NUB + F51M, # business wealth
     FW = F3 + F511 + F52 + F62, # financial wealth
     DEP = F2M, # deposits
@@ -120,10 +77,9 @@ df_germany <- df %>%
     TW = BW + FW + HW + DEP,
     DEBT = F4X + F4B, # total debt
     TW_NET = BW + FW + DEP + HW + DEBT, # total wealth net
-  ) %>%
-  select(
-    TIME_PERIOD, DWA_GRP, UNIT_MEASURE,
-    BW, FW, DEP, HW, HW_NET, TW, DEBT, TW_NET
-  )
+  ) 
 
-write_csv(df_germany, "../data/DWA_DE.csv")
+# Save the simplified data
+write_csv(df_filter, "../data/DWA_ECB_simplified.csv")
+
+
